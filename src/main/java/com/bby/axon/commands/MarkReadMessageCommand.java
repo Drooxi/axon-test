afdaca67a54550507483ab607ff9dc19d2a07e4a
Drooxi <beranger.bauby@outlook.fr>
2018-03-02 13:37:39 +0100

    Creating the commands

---
+++ src/main/java/com/bby/axon/commands/MarkReadMessageCommand.java
@@ -0,0 +1,16 @@
+package com.bby.axon.commands;
+
+import org.axonframework.commandhandling.TargetAggregateIdentifier;
+
+public class MarkReadMessageCommand {
+    @TargetAggregateIdentifier
+    private final String id;
+
+    public MarkReadMessageCommand(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
