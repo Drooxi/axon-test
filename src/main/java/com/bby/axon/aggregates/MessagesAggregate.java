5b8a28c085286d0627b11238b7f8e6edfe166f1f
Drooxi <beranger.bauby@outlook.fr>
2018-03-04 16:07:51 +0100

    Creating the aggregates

---
+++ src/main/java/com/bby/axon/aggregates/MessagesAggregate.java
@@ -0,0 +1,36 @@
+package com.bby.axon.aggregates;
+
+import com.bby.axon.commands.CreateMessageCommand;
+import com.bby.axon.commands.MarkReadMessageCommand;
+import com.bby.axon.events.MessageCreatedEvent;
+import com.bby.axon.events.MessageReadEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.eventhandling.EventHandler;
+
+import static org.axonframework.commandhandling.model.AggregateLifecycle.apply;
+
+public class MessagesAggregate {
+
+    @AggregateIdentifier
+    private String id;
+
+    public MessagesAggregate() {
+    }
+
+    @CommandHandler
+    public MessagesAggregate(CreateMessageCommand command) {
+        apply(new MessageCreatedEvent(command.getId(), command.getText()));
+    }
+
+    @EventHandler
+    public void on(MessageCreatedEvent event) {
+        this.id = event.getId();
+    }
+
+    @CommandHandler
+    public void markRead(MarkReadMessageCommand command) {
+        apply(new MessageReadEvent(id));
+    }
+
+}
